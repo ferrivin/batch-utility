5bf5f0e1314252fe9628dd8930cf4d9bc244dc17
ferrivin <68539220+ferrivin@users.noreply.github.com>
2022-04-18 00:04:52 +0200

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    char chr;
+    printf("Socioooo! Il 30 settembre mi sposo e faccio il battesimo a Nina");
+    printf("Mi piacerebbe molto averti come testimone. Ti farebbe piacere? [Y/N]");
+    scanf("%c",&chr);     
+    printf("You entered %c.", chr);  
+    return 0;
+}   
